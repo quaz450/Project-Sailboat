aa437e2f6d9562febb738654886a1fa7cffb87eb
Ben Parker <parker.benjaminr@gmail.com>
2018-12-01 20:19:07 -0400

    Update Assets/LaserBoat.fbx

---
+++ Assets/Scripts/WindDirection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WindDirection : MonoBehaviour {
+
+    public float windDirection;
+
+	// Use this for initialization
+	void Start () {
+        windDirection = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizontal") < 0)
+        { 
+            if (windDirection <= 0)
+                windDirection = 360;
+            windDirection = (windDirection - 1)*Time.deltaTime;
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        { 
+            if (windDirection >= 360)
+                windDirection = 0;
+            windDirection = (windDirection + 1) * Time.deltaTime;
+        }
+
+    }
+
+    public float getWindDirection()
+    {
+        return windDirection;
+    }
+}
